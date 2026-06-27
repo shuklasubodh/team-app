c9c12576145fc23d484f1e99f506c5c97c772e23
yingtong <yingtong2307@gmail.com>
2026-06-27 12:08:33 +0800

    add in link to NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,35 @@
+import { Link } from "react-router-dom";
+
 function NavBar() {
   return (
     <>
-      <h2>NavBar</h2>
+      <h2 style={{ marginBottom: "0.5rem" }}>NavBar</h2>
       <div>buttons for navigation here</div>
+      <ul
+        style={{
+          display: "flex",
+          gap: "1rem",
+          listStyle: "none",
+          padding: 0,
+          margin: 0,
+        }}
+      >
+        <li>
+          <Link to="/welcome">Welcome</Link>
+        </li>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/settings">Settings</Link>
+        </li>
+      </ul>
     </>
   );
 }
