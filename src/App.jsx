a5961656631ee39f5d32a391141dfc4a015eaaa0
Johnny <chuaboonhock@singnet.com.sg>
2026-06-27 10:50:34 +0800

    add App.jsx to feature/johnny

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Welcome from "./components/Welcome";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <div>
+      <Welcome />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
