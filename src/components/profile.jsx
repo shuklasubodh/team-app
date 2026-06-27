493cc528a6e7cf8d0958df5eed720be34d44e761
Johnny <chuaboonhock@singnet.com.sg>
2026-06-27 10:44:47 +0800

    adding profile.jsx and welcome.jsx

---
+++ src/components/profile.jsx
@@ -0,0 +1,9 @@
+function profile() {
+  return (
+    <div>
+      <h2> Our Profile - Team from SB, YT and JC</h2>
+    </div>
+  );
+}
+
+export default profile;
---
+++ src/components/welcome.jsx
@@ -0,0 +1,9 @@
+function welcome() {
+  return (
+    <div>
+      <h1>Welcome to Team 2 test project</h1>
+    </div>
+  );
+}
+
+export default welcome;
